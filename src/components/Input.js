aba88284fe1d31f244fa464259d80404cad365a8
luckmer <woszczo04@gmail.com>
2021-01-16 21:29:17 +0100

    update

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,30 +1,61 @@
-import { useState } from "react";
-import { CollectData } from "../reducers/DataSlice";
+import { useState,useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { CollectData,AddData } from "../reducers/DataSlice";
 import { nanoid } from "@reduxjs/toolkit";
-
-import { useDispatch } from "react-redux";
+import {Api} from "../api/Api"
+import { Article,Card,Form,Button } from "../styles/Input.Style";
 const Input = () =>{
-    const [filter, setFilter] = useState("");
+    const [filter, setFilter] = useState({ filterSubject: "" });
     const dispatch = useDispatch()
+    const data = useSelector(state => state.Data)
+    let location = data.container.filter
 
     const handleSubmit = (e) =>{
         e.preventDefault();
-        const data = { id: nanoid(), filter };
-        dispatch(CollectData(data));
+        if (typeof filter.filterSubject === "string" && filter.filterSubject.length >= 3) {
+            const data = { id: nanoid(), filter: filter.filterSubject };
+            dispatch(CollectData(data));
+            setFilter({filterSubject:""})
+        } 
     };
 
+    useEffect(()=> {
+        if (typeof location === 'undefined') return 
+        const data = async () =>{
+            const context = await Api(location);
+            dispatch(AddData(context));
+        }
+        data(); 
+    }, [location, dispatch]);
+
+    const handleChange = (e) => setFilter({ ...filter, [e.target.name]: e.target.value });
+
     const InputForm = (
-        <form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit}>
             <input
-                value={filter}
-                type="text"
-                name="input"
-                onChange={(e) => setFilter(e.target.value)} />
-            <button type="submit">submit</button>
-        </form>
+            type="text"
+            name="filterSubject"
+            value={filter.filterSubject}
+            onChange={handleChange}
+            />
+            <Button type="submit" data-text="GITHUB">
+                <span data-text ="Submit">Submit</span>
+            </Button>
+        </Form>
     );
 
-    return InputForm
+
+
+    return (
+        <Article>
+            <div>
+                <Card>
+                    {InputForm}
+                </Card>
+            </div>
+        </Article>
+    )
 }
 
 export default Input
+
