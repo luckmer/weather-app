e453f39f0b97634fba678b6c636d90e4a339a6a5
luckmer <woszczo04@gmail.com>
2021-01-17 15:45:33 +0100

    reducers update

--- src/reducers/DataSlice.js
+++ src/reducers/DataSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 let initialState = {
     container: "",
     DisplayData: "",
+    DisplayForeCast:""
 };
 
 const DataSlice = createSlice({
@@ -17,8 +18,12 @@ const DataSlice = createSlice({
             const contextData = action.payload;
             state.DisplayData = contextData
         },
+        AddForeCast: (state, action) =>{            
+            const data = action.payload;
+            state.DisplayForeCast = data
+        }
     }   
 });
 
-export const { CollectData,AddData } = DataSlice.actions;
+export const { CollectData,AddData,AddForeCast } = DataSlice.actions;
 export default DataSlice.reducer;
