890e69fe4c74818970b52f91080ab350fee91bc2
luckmer <woszczo04@gmail.com>
2021-01-17 15:45:54 +0100

    .

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,25 +1,25 @@
 import { createStore } from "redux";
 import Reducer from "../reducers/Index";
 
-// const Save = (state) =>
-// {
-//     try {
-//         const Save = JSON.stringify(state);
-//         localStorage.setItem("DATA", Save);
-//     } catch (err) {
-//         throw err
-//     }
-// }
-// const Load = () =>{
-//     try {
-//         const load = localStorage.getItem("DATA");
-//         if (load === null) return undefined;
-//         return JSON.parse(load);
-//     } catch (err) {
-//         throw err
-//     }
-// }
-const Store = createStore(Reducer);
-// Store.subscribe(() => Save(Store.getState()));
+const Save = (state) =>
+{
+    try {
+        const Save = JSON.stringify(state);
+        localStorage.setItem("DATA", Save);
+    } catch (err) {
+        throw err
+    }
+}
+const Load = () =>{
+    try {
+        const load = localStorage.getItem("DATA");
+        if (load === null) return undefined;
+        return JSON.parse(load);
+    } catch (err) {
+        throw err
+    }
+}
+const Store = createStore(Reducer,Load());
+Store.subscribe(() => Save(Store.getState()));
 
 export default Store
