c63254047a93666ae4bf30d8f45e0e242b4bcaa0
luckmer <woszczo04@gmail.com>
2021-01-18 19:17:39 +0100

    filter by correct data

--- src/api/Api.js
+++ src/api/Api.js
@@ -8,28 +8,33 @@ export const Api = async (location) =>{
     }
 }
 export const fiveDayForecast = async (location) =>{
-
-    const API = () => `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${ process.env.REACT_APP_API_KEY }`
+    const API = () => `https://api.openweathermap.org/data/2.5/forecast?q=${ location }&appid=${ process.env.REACT_APP_API_KEY }`
     
     try {
         
-        const resp = await (await fetch(API(location), { origin: "cors" })).json();
+        const  resp = await (await fetch(API(location), { origin: "cors" })).json();
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const values = [0, 7, 15, 23, 31, 39];
+
+        let filteredData = new Date(resp.list[0].dt * 1000);
+        let dayName = days[filteredData.getDay()];
         let List = resp.list;
-        let dailyData ={}
+        let dailyData = [];
 
-        List.map(item =>{
-            const dateTime = new Date(item.dt * 1000 );
-            const day = dateTime.getDate();
-            const time = dateTime.getHours();
-
-            if (!dailyData[day]) {
-                dailyData[day]=[];
-                dailyData[day].push({...item,day,time});    
+        List.map((index, val) =>{
+            if (values.includes(val)) {
+                filteredData = new Date(resp.list[val].dt * 1000);
+                dayName = days[filteredData.getDay()];
+                
+                let day = dayName;
+                let img = `https://openweathermap.org/img/w/${ index.weather[0].icon }.png`;
+                let total = { ...index, day, img };
+                dailyData.push(total);
             }
             return dailyData
         });
         return dailyData
-
+        
     } catch (err) {
         throw err
     }
