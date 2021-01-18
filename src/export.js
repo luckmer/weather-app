49fa06b579004d728b8fb43c4faf48f851903c39
luckmer <woszczo04@gmail.com>
2021-01-18 19:20:38 +0100

    tiny export fix

--- src/export.js
+++ src/export.js
@@ -1,3 +1,4 @@
 export { default as Input } from "./components/Input";
 export { default as Index } from "./pages/Index";
 export { default as Weather } from "./components/Weather";
+export { default as NavBar } from "./components/NavBar";
