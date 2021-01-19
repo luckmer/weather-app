dfaa4420c3435373196479649b689c8b9a98a8d9
luckmer <woszczo04@gmail.com>
2021-01-19 21:56:34 +0100

    components for calculations

--- src/components/TempCalculate.js
+++ src/components/TempCalculate.js
@@ -1,19 +1,32 @@
 
-const  TempCalculate = (countryData) => {
-    let data  = countryData.main.temp;
-    let temp = parseFloat(data);
-
+export const  TempCalculate = ({temp,speed}) => {
+    let test = parseFloat(temp);
 
     let Celcius;
     let Fahrenheit;
     let Kelvin;
+    Celcius =  Math.round(test - 273.15) 
+    // Fahrenheit = Math.round(Celcius * 9 / 5 + 32)
+    // Kelvin = Math.round(Celcius + 273.15)
 
-
-    Celcius =  Math.round(temp - 273.15) 
-    Fahrenheit = Math.round(Celcius * 9 / 5 + 32)
-    Kelvin = Math.round(Celcius + 273.15)
+    return <>{Celcius} &#176;</>
     
-    return { Celcius, Fahrenheit, Kelvin };
 }
 
-export default TempCalculate
+
+export const  WindSpeed = ({speed}) => {
+
+    let WindSpeed;
+    WindSpeed = Math.round(0.836 * Math.sqrt(Math.pow(speed, 3)) * 100) / 100;
+    return <>{WindSpeed} </>
+    
+}
+
+export const  Visibility = ({visibility}) => {
+
+    let Visibile;
+    Visibile = Math.floor(visibility / 1000);
+
+    return <>{Visibile} km </>
+    
+}
