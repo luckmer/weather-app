b970392bacd3d9d61bdce65962c0c746dbd916f5
luckmer <woszczo04@gmail.com>
2021-01-19 22:04:40 +0100

    update

--- src/export.js
+++ src/export.js
@@ -1,4 +1,4 @@
-export { default as Input } from "./components/Input";
+export { default as Input } from "./pages/Input";
 export { default as Index } from "./pages/Index";
-export { default as Weather } from "./components/Weather";
+export { default as Weather } from "./pages/Weather";
 export { default as NavBar } from "./components/NavBar";
