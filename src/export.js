930fa304c984fbbd99a645b134d4a7fbea46cf2a
luckmer <woszczo04@gmail.com>
2021-01-20 15:57:49 +0100

    export update

--- src/export.js
+++ src/export.js
@@ -2,3 +2,5 @@ export { default as Input } from "./pages/Input";
 export { default as Index } from "./pages/Index";
 export { default as Weather } from "./pages/Weather";
 export { default as NavBar } from "./components/NavBar";
+export { default as Graph } from "./components/Graph";
+export {default as FilterCurrentDay} from "./hooks/FilterCurrectDay"
